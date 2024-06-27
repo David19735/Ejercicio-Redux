997a2f7123d8dde5d3f29554661448bdaf9ea318
David19735 <42193921+David19735@users.noreply.github.com>
2024-06-27 14:08:43 -0600

    Iniciando proyecto de Redux con react

---
+++ src/componentes/Blog.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Blog = () => {
+    return (
+        <div>
+            <h1>Blog</h1>
+            <p>Aquí es la parte del blog</p>
+        </div>
+      );
+}
+ 
+export default Blog;
---
+++ src/componentes/Inicio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Inicio = () => {
+    return (  
+        <div>
+            <h1>Inicio</h1>
+            <p>Este es el inicio de la página</p>
+        </div>
+    );
+}
+ 
+export default Inicio;
